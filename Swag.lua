24fa6013cf1d36dc6c4e617ead4fcec13fb20e7c
swagcodeing2 <200179763+swagcodeing2@users.noreply.github.com>
2025-10-27 19:20:27 -0700

    Update and rename Swag2.lua to Swag.lua

--- Swag.lua
+++ Swag.lua
@@ -30,7 +30,7 @@ function NotificationLibrary:Notify(TitleText, Desc, Delay)
 	Warning.BackgroundTransparency = 1.000
 	Warning.Position = UDim2.new(0.0258302614, 0, 0.0897435844, 0)
 	Warning.Size = UDim2.new(0, 44, 0, 49)
-	Warning.Image = "rbxassetid://93453832881348"
+	Warning.Image = "rbxassetid://6031071053""
 	Warning.ImageColor3 = Color3.fromRGB(241, 196, 15)
 	Warning.ScaleType = Enum.ScaleType.Fit
 
